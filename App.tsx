eca9dffa4d504fede6827db60996677fddba478a
SaideLeon <saideomarsaid@gmail.com>
2025-10-01 00:35:06 +0200

    refactor: Adjust UI layout for better responsiveness
    Updates CSS to improve the positioning and responsiveness of UI elements, particularly the transcription container. Also, refactors `App.tsx` to remove unnecessary curly braces for component rendering.

--- App.tsx
+++ App.tsx
@@ -7,8 +7,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may
- * obtain a copy of the License at
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -120,7 +119,7 @@ function App() {
                 )}
               </div>
 
-              {isAuthenticated && !showTermsModal && currentView === 'console' && <ControlTray></ControlTray>}
+              {isAuthenticated && !showTermsModal && currentView === 'console' && <ControlTray />}
             </main>
           </div>
         </div>
